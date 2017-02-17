82aa50e04aaa6fb45cb94b0b4cc4ce2d9947826c
desustanciao <angelcollas@gmail.com>
2017-02-17 10:13:43 +0100

    poker almost solved

--- 10315-poker.py
+++ 10315-poker.py
@@ -2,12 +2,38 @@ import sys
 
 def calculate_hand(hand):
     valor = 0
-    #comprobamos que sea escalera y escalera de color
+    max_hand = max(hash_map[x[0]] for x in hand)
+    #escalera de color
     if hand[0][1] == hand[1][1] == hand[2][1] == hand[3][1] == hand[4][1] \
-            and hash_map[hand[0][0]] == hash_map[hand[1][0]]-1 == hash_map[hand[2][0]]-2 == hash_map[hand[3][0]]-3 == hash_map[hand[4][0]]-4:
-            return 900+max(hash_map[x[0]] for x in hand)
-    elif max(dict, key=lambda key: dict_black[key])
-
+        and hash_map[hand[0][0]] == hash_map[hand[1][0]]-1 == hash_map[hand[2][0]]-2 == \
+            hash_map[hand[3][0]]-3 == hash_map[hand[4][0]]-4:
+        return 900+max_hand
+    #poker
+    elif max(dict, key=lambda key: dict[key]) == 4:
+        return 800+max_hand
+    #full
+    elif max(dict, key=lambda key: dict[key]) == 3 \
+        and min(dict, key=lambda key: dict[key]) == 2:
+        return 700+max_hand
+    #color
+    elif hand[0][1] == hand[1][1] == hand[2][1] == hand[3][1] == hand[4][1]:
+        return 600+max_hand
+    #escalera
+    elif hash_map[hand[0][0]] == hash_map[hand[1][0]]-1 == hash_map[hand[2][0]]-2 == \
+            hash_map[hand[3][0]]-3 == hash_map[hand[4][0]]-4:
+        return 500+max_hand
+    #trio
+    elif max(dict, key=lambda key: dict[key]) == 3:
+        return 400+max_hand
+    #doble pareja
+    elif list(dict.values()).count('2') == 2:
+        return 300+max_hand
+    #pareja
+    elif list(dict.values()).count('2') == 1:
+        return 200+max_hand
+    #carta mas alta
+    else:
+        return 100+max_hand
     return ""
 
 if __name__ == '__main__':
@@ -25,10 +51,17 @@ if __name__ == '__main__':
         white = hands[5:10]
         for x in black:
             dict[x[0]] += 1
-
+        points_black = calculate_hand(black)
         dict = {"A": 0, "K": 0, "Q": 0, "J": 0, "T": 0, "9": 0, "8": 0, "7": 0, "6": 0, "5": 0, "4": 0,
                 "3": 0, "2": 0}
         for x in white:
             dict[x[0]] += 1
+        points_white = calculate_hand(white)
 
+        if points_black > points_white:
+            print('Black Wins.')
+        elif points_white > points_black:
+            print('White wins.')
+        else:
+            print('Tie.')
 
