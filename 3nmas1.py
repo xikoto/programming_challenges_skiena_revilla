3229775d0f5a64c7a8c534f5b2662d4771ad7b76
desustanciao <angelcollas@gmail.com>
2017-02-28 12:00:03 +0100

    3n+1 y ones

---
+++ 3nmas1.py
@@ -0,0 +1,35 @@
+import sys
+
+def algorithm(n):
+    count = 1
+    while n > 1:
+        count += 1
+        if n % 2 == 0:
+            n = n / 2
+        else:
+            n = 3 * n + 1
+    return count
+
+
+def exercise(i, j):
+    n1 = min(i, j)
+    n2 = max(i, j)
+    result = 0
+    for k in range(n1, n2 + 1):
+        result = max(algorithm(k), result)
+    return result
+
+
+if __name__ == '__main__':
+    final = ""
+    for lin in sys.stdin:
+        lin = lin.rstrip('\n')
+        if len(lin.split()) < 2:
+            i = int(lin.split()[0])
+            j = i
+        else:
+            i = int(lin.split()[0])
+            j = int(lin.split()[1])
+        final += str(i) + ' ' + str(j) + ' ' + str(exercise(i, j)) + '\n'
+    final = final[0:len(final) - 1]
+    print(final)
